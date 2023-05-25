876d6cd34d3c50caad0069e25d5cc2aa2a6692f1
Werter Pires <werterpires23@hotmail.com>
2023-05-25 19:24:26 -0300

    add backend courses

---
+++ backend/src/modules/info/courses/controllers/courses.controller.ts
@@ -0,0 +1,122 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common'
+import {CoursesService} from '../services/courses.service'
+import {NotFoundException, InternalServerErrorException} from '@nestjs/common'
+import {CurrentUser} from 'src/shared/auth/decorators/current-user.decorator'
+import {ERoles} from 'src/shared/auth/types/roles.enum'
+import {UserFromJwt} from 'src/shared/auth/types/types'
+import {Roles} from 'src/shared/roles/fz_decorators/roles.decorator'
+import {CreateCourseDto} from '../dto/create-course.dto'
+import {UpdateCourseDto} from '../dto/update-course.dto'
+import {ICourse} from '../types/types'
+
+@Controller('courses')
+export class CoursesController {
+  constructor(private coursesService: CoursesService) {}
+
+  @Roles(ERoles.ADMINISTRACAO, ERoles.ESTUDANTE)
+  @Post(':personType')
+  async createCourse(
+    @Body() input: CreateCourseDto,
+    @CurrentUser() user: UserFromJwt,
+    @Param('personType') personType: string,
+  ) {
+    try {
+      const user_id = user.user_id
+      if (personType !== 'student' && personType !== 'spouse') {
+        throw new Error('End point inválido.')
+      }
+      const course = await this.coursesService.createCourse(
+        input,
+        user_id,
+        personType,
+      )
+      return course
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  @Roles(ERoles.ADMINISTRACAO, ERoles.ESTUDANTE)
+  @Get('person/:personType')
+  async findCoursesByPersonId(
+    @CurrentUser() user: UserFromJwt,
+    @Param('personType') personType: string,
+  ): Promise<ICourse[]> {
+    try {
+      const user_id = user.user_id
+      if (personType !== 'student' && personType !== 'spouse') {
+        throw new Error('End point inválido.')
+      }
+      const courses = await this.coursesService.findCoursesByPersonId(
+        user_id,
+        personType,
+      )
+
+      if (!courses) {
+        throw new NotFoundException(
+          `No courses found for person with id fornecido.`,
+        )
+      }
+      return courses
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  @Roles(ERoles.ADMINISTRACAO, ERoles.ESTUDANTE)
+  @Get(':id')
+  async findCourseById(@Param('id') id: number): Promise<ICourse> {
+    try {
+      console.log('rota correta')
+      const course = await this.coursesService.findCourseById(id)
+      if (!course) {
+        throw new NotFoundException(`No course found with id ${id}.`)
+      }
+      return course
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  @Roles(ERoles.ADMINISTRACAO, ERoles.ESTUDANTE, ERoles.SECRETARIA)
+  @Get()
+  async findAllCourses(): Promise<ICourse[]> {
+    try {
+      const courses = await this.coursesService.findAllCourses()
+      return courses
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  @Roles(ERoles.ADMINISTRACAO, ERoles.ESTUDANTE)
+  @Put()
+  async updateCourseById(@Body() input: UpdateCourseDto): Promise<ICourse> {
+    try {
+      const updatedCourse = await this.coursesService.updateCourseById(input)
+      return updatedCourse
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+
+  @Roles(ERoles.ADMINISTRACAO)
+  @Delete(':id')
+  async deleteCourseById(@Param('id') id: number) {
+    try {
+      const message = await this.coursesService.deleteCourseById(id)
+      return {message}
+    } catch (error) {
+      throw new InternalServerErrorException(error.message)
+    }
+  }
+}
---
+++ backend/src/modules/info/courses/model/courses.model.ts
@@ -0,0 +1,216 @@
+import {Injectable} from '@nestjs/common'
+import {Knex} from 'knex'
+import {InjectModel} from 'nest-knexjs'
+import {ICreateCourse, ICourse, IUpdateCourse} from '../types/types'
+
+@Injectable()
+export class CoursesModel {
+  constructor(@InjectModel() private readonly knex: Knex) {}
+
+  async createCourse(createCourseData: ICreateCourse): Promise<ICourse> {
+    let course: ICourse | null = null
+    let sentError: Error | null = null
+
+    await this.knex.transaction(async (trx) => {
+      try {
+        const {
+          course_area,
+          institution,
+          begin_date,
+          conclusion_date,
+          person_id,
+          course_approved,
+        } = createCourseData
+
+        const [result] = await trx('courses').insert({
+          course_area,
+          institution,
+          begin_date,
+          conclusion_date,
+          person_id,
+          course_approved,
+        })
+
+        await trx.commit()
+
+        course = await this.findCourseById(result)
+      } catch (error) {
+        await trx.rollback()
+        if (error.code === 'ER_DUP_ENTRY') {
+          sentError = new Error('Course already exists')
+        } else {
+          sentError = new Error(error.sqlMessage)
+        }
+      }
+    })
+
+    if (sentError) {
+      throw sentError
+    }
+
+    return course!
+  }
+
+  async findCourseById(id: number): Promise<ICourse | null> {
+    let course: ICourse | null = null
+    let sentError: Error | null = null
+
+    await this.knex.transaction(async (trx) => {
+      try {
+        const result = await trx
+          .table('courses')
+          .where('course_id', '=', id)
+          .first()
+
+        if (!result) {
+          throw new Error('Course not found')
+        }
+
+        course = {
+          course_id: result.course_id,
+          course_area: result.course_area,
+          institution: result.institution,
+          begin_date: result.begin_date,
+          conclusion_date: result.conclusion_date,
+          person_id: result.person_id,
+          course_approved: result.course_approved,
+          created_at: result.created_at,
+          updated_at: result.updated_at,
+        }
+
+        await trx.commit()
+      } catch (error) {
+        sentError = new Error(error.message)
+        await trx.rollback()
+        throw error
+      }
+    })
+
+    if (sentError) {
+      throw sentError
+    }
+
+    return course
+  }
+
+  async findCoursesByPersonId(personId: number): Promise<ICourse[]> {
+    let courseList: ICourse[] = []
+    let sentError: Error | null = null
+
+    await this.knex.transaction(async (trx) => {
+      try {
+        courseList = await trx
+          .table('courses')
+          .where('person_id', '=', personId)
+          .select('*')
+
+        await trx.commit()
+      } catch (error) {
+        sentError = new Error(error.sqlMessage)
+        await trx.rollback()
+      }
+    })
+
+    if (sentError) {
+      throw sentError
+    }
+
+    return courseList
+  }
+
+  async findAllCourses(): Promise<ICourse[]> {
+    let courseList: ICourse[] = []
+    let sentError: Error | null = null
+
+    await this.knex.transaction(async (trx) => {
+      try {
+        courseList = await trx.table('courses').select('*')
+
+        await trx.commit()
+      } catch (error) {
+        sentError = new Error(error.sqlMessage)
+        await trx.rollback()
+      }
+    })
+
+    if (sentError) {
+      throw sentError
+    }
+
+    return courseList
+  }
+
+  async updateCourseById(updateCourse: IUpdateCourse): Promise<ICourse> {
+    let updatedCourse: ICourse | null = null
+    let sentError: Error | null = null
+
+    await this.knex.transaction(async (trx) => {
+      try {
+        const {
+          course_id,
+          course_area,
+          institution,
+          begin_date,
+          conclusion_date,
+          person_id,
+          course_approved,
+        } = updateCourse
+
+        await trx('courses').where('course_id', course_id).update({
+          course_area,
+          institution,
+          begin_date,
+          conclusion_date,
+          person_id,
+          course_approved,
+        })
+
+        await trx.commit()
+
+        updatedCourse = await this.findCourseById(course_id)
+      } catch (error) {
+        await trx.rollback()
+        sentError = new Error(error.message)
+      }
+    })
+
+    if (sentError) {
+      throw sentError
+    }
+
+    return updatedCourse!
+  }
+
+  async deleteCourseById(id: number): Promise<string> {
+    let sentError: Error | null = null
+    let message: string = ''
+
+    await this.knex.transaction(async (trx) => {
+      try {
+        const existingCourse = await trx('courses')
+          .select('course_id')
+          .where('course_id', id)
+          .first()
+
+        if (!existingCourse) {
+          throw new Error('Course not found')
+        }
+
+        await trx('courses').where('course_id', id).del()
+
+        await trx.commit()
+      } catch (error) {
+        await trx.rollback()
+        console.log(error)
+        sentError = new Error(error.message)
+      }
+    })
+
+    if (sentError) {
+      throw sentError
+    }
+
+    message = 'Course deleted successfully.'
+    return message
+  }
+}
