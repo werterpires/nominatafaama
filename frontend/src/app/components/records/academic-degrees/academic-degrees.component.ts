c03f4270f4222b3cfad08b3f97f94aa91595947d
F4289001 Hugo Sant Ana Guimaraes <hugo.santana@bb.com.br>
2023-05-25 14:24:25 -0300

    fix padroniza academic degree

--- frontend/src/app/components/records/academic-degrees/academic-degrees.component.ts
+++ frontend/src/app/components/records/academic-degrees/academic-degrees.component.ts
@@ -1,148 +1,181 @@
-import { Component, Input, Renderer2 } from '@angular/core';
-import { AcademicDegreeService } from './academic-degrees.service';
-import { IPermissions } from '../../shared/container/types';
-import { IAcademicDegree, ICreateAcademicDegreeDto, IUpdateAcademicDegree } from './types';
+import {Component, Input, Renderer2} from '@angular/core'
+import {IPermissions} from '../../shared/container/types'
+import {DialogService} from '../../shared/shared.service.ts/dialog.service'
+import {AcademicDegreeService} from './academic-degrees.service'
+import {
+  IAcademicDegree,
+  ICreateAcademicDegreeDto,
+  IUpdateAcademicDegree,
+} from './types'
 
 @Component({
   selector: 'app-academic-degrees',
   templateUrl: './academic-degrees.component.html',
-  styleUrls: ['./academic-degrees.component.css']
+  styleUrls: ['./academic-degrees.component.css'],
 })
 export class AcademicDegreesComponent {
   constructor(
-    private academicDegreesService:AcademicDegreeService,
-    private renderer: Renderer2){}
+    private academicDegreesService: AcademicDegreeService,
+    private renderer: Renderer2,
+    private dialogService: DialogService,
+  ) {}
 
   @Input() permissions!: IPermissions
-  allDegrees:IAcademicDegree[] = []
-  creatingDegree:boolean = false
-  editingDegree:boolean = false
-  createAcademicDegreeData:ICreateAcademicDegreeDto = {
-    degree_name: ""
+  allDegrees: IAcademicDegree[] = []
+  creatingDegree: boolean = false
+  editingDegree: boolean = false
+  createAcademicDegreeData: ICreateAcademicDegreeDto = {
+    degree_name: '',
   }
 
   isLoading: boolean = false
-  done:boolean=false
-  doneMessage:string = ''
-  error:boolean = false
-  errorMessage:string = ''
+  done: boolean = false
+  doneMessage: string = ''
+  error: boolean = false
+  errorMessage: string = ''
 
-  shownBox:boolean=false
+  shownBox: boolean = false
 
-  ngOnInit(){
+  ngOnInit() {
     this.isLoading = true
     this.academicDegreesService.findAllAcademicDegrees().subscribe({
-      next: res => {
-        this.allDegrees= res
+      next: (res) => {
+        this.allDegrees = res
         this.isLoading = false
       },
-      error: err => {
+      error: (err) => {
         this.errorMessage = err.message
         this.error = true
         this.isLoading = false
-      }
-    });
+      },
+    })
   }
 
-  showBox(){
-    const box = document.getElementById("boxHeadAcademicDegres");
-    const add = document.getElementById("degreeAddIcon")
-    const see = document.getElementById("seeMoreIconDegrees")
+  showBox() {
+    const box = document.getElementById('boxHeadAcademicDegres')
+    const add = document.getElementById('degreeAddIcon')
+    const see = document.getElementById('seeMoreIconDegrees')
     this.shownBox = !this.shownBox
-    if (this.shownBox){
-      box?.classList.replace("smallSectionBox", "sectionBox")
-      add?.classList.remove("hidden")
-      see?.classList.add("rotatedClock")
+    if (this.shownBox) {
+      box?.classList.replace('smallSectionBox', 'sectionBox')
+      add?.classList.remove('hidden')
+      see?.classList.add('rotatedClock')
       this.editingDegree = false
-    }else{
-      box?.classList.replace("sectionBox", "smallSectionBox")
-      add?.classList.add("hidden")
-      see?.classList.remove("rotatedClock")
-
+    } else {
+      box?.classList.replace('sectionBox', 'smallSectionBox')
+      add?.classList.add('hidden')
+      see?.classList.remove('rotatedClock')
     }
   }
 
-  createForm(){
+  createForm() {
     this.creatingDegree = true
   }
 
-  createAcademicDegree(){
+  createAcademicDegree() {
     this.isLoading = true
-    this.academicDegreesService.createAcademicDegree(this.createAcademicDegreeData).subscribe({
-      next: res => {
-        this.doneMessage = 'Grau Acadêmico criado com sucesso.'
-        this.done = true
-        this.isLoading = false
-        this.ngOnInit()
-        this.createAcademicDegreeData.degree_name = ""
-        this.creatingDegree = false
-
-      },
-      error: err => {
-        this.errorMessage = err.message
-        this.error = true
-        this.isLoading = false
-      }
-    });
+    this.academicDegreesService
+      .createAcademicDegree(this.createAcademicDegreeData)
+      .subscribe({
+        next: (res) => {
+          this.doneMessage = 'Grau Acadêmico criado com sucesso.'
+          this.done = true
+          this.isLoading = false
+          this.ngOnInit()
+          this.createAcademicDegreeData.degree_name = ''
+          this.creatingDegree = false
+        },
+        error: (err) => {
+          this.errorMessage = err.message
+          this.error = true
+          this.isLoading = false
+        },
+      })
   }
 
-  changeTagType(paragraphId:string, buttonId:string, inputId:string) {
-    const paragraph = document.getElementById(paragraphId);
-    const input = document.getElementById(inputId) as HTMLInputElement;
-    
-    if(paragraph !== null && paragraph.textContent && input!==null){
+  changeTagType(paragraphId: string, buttonId: string, inputId: string) {
+    const paragraph = document.getElementById(paragraphId)
+    const input = document.getElementById(inputId) as HTMLInputElement
+
+    if (paragraph !== null && paragraph.textContent && input !== null) {
       input.classList.remove('hidden')
       paragraph.classList.add('hidden')
 
-      input.value = paragraph.textContent;     
-      input.oninput = function(){
-        const button = document.getElementById(buttonId)?.classList.remove('hidden')
+      input.value = paragraph.textContent
+      input.oninput = function () {
+        const button = document
+          .getElementById(buttonId)
+          ?.classList.remove('hidden')
       }
 
-      input.focus();
+      input.focus()
 
-      input.onblur = function() {
-  
-        paragraph.textContent = input.value;
+      input.onblur = function () {
+        paragraph.textContent = input.value
         input.classList.add('hidden')
         paragraph.classList.remove('hidden')
       }
-   
-    
-    };
-    
+    }
   }
 
-  editAcademicDegree(i:number, buttonId:string){
+  editAcademicDegree(i: number, buttonId: string) {
     this.isLoading = true
-    const editDegreeData:IUpdateAcademicDegree = {
+    const editDegreeData: IUpdateAcademicDegree = {
       degree_id: this.allDegrees[i].degree_id,
-      degree_name: this.allDegrees[i].degree_name
+      degree_name: this.allDegrees[i].degree_name,
     }
 
     this.academicDegreesService.editAcademicDegree(editDegreeData).subscribe({
-      next: res => {
+      next: (res) => {
         this.doneMessage = 'Grau acadêmico editado com sucesso.'
         this.done = true
-        const button = document.getElementById(buttonId)?.classList.add('hidden')
+        const button = document
+          .getElementById(buttonId)
+          ?.classList.add('hidden')
         this.isLoading = false
-
       },
-      error: err => {
+      error: (err) => {
         this.errorMessage = err.message
         this.error = true
         this.isLoading = false
-      }
+      },
     })
-
   }
 
-  closeError(){
+  deleteAcademicDecree(i: number) {
+    this.dialogService
+      .new('Confirma a remoção?', [
+        'Você tem certeza de que deseja remover este registro?',
+      ])
+      .then((confirmation) => {
+        if (this.dialogService.checkConfirmation(confirmation)) {
+          this.isLoading = true
+          const publicationTypeId = this.allDegrees[i].degree_id
+
+          this.academicDegreesService
+            .deleteAcademicDegree(publicationTypeId)
+            .subscribe({
+              next: (res) => {
+                this.doneMessage = 'Registro deletado com sucesso.'
+                this.done = true
+                this.isLoading = false
+                this.ngOnInit()
+              },
+              error: (err) => {
+                this.errorMessage = err.message
+                this.error = true
+                this.isLoading = false
+              },
+            })
+        }
+      })
+  }
+
+  closeError() {
     this.error = false
   }
 
-  closeDone(){
+  closeDone() {
     this.done = false
   }
-
 }
--- frontend/src/app/components/records/academic-degrees/academic-degrees.service.ts
+++ frontend/src/app/components/records/academic-degrees/academic-degrees.service.ts
@@ -1,58 +1,96 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { IAcademicDegree, ICreateAcademicDegreeDto, IUpdateAcademicDegree } from './types';
-
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Injectable} from '@angular/core'
+import {Router} from '@angular/router'
+import {Observable, throwError} from 'rxjs'
+import {catchError} from 'rxjs/operators'
+import {
+  IAcademicDegree,
+  ICreateAcademicDegreeDto,
+  IUpdateAcademicDegree,
+} from './types'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AcademicDegreeService {
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) { };
-
-  findAllAcademicDegrees(): Observable<IAcademicDegree[]>{
-    const token = localStorage.getItem('access_token');
-    let head_obj = new HttpHeaders().set("Authorization", "bearer " + token);
-    return this.http.get<IAcademicDegree[]>('http://localhost:3000/academic-degrees', { headers: head_obj })
+  findAllAcademicDegrees(): Observable<IAcademicDegree[]> {
+    const token = localStorage.getItem('access_token')
+    let head_obj = new HttpHeaders().set('Authorization', 'bearer ' + token)
+    return this.http
+      .get<IAcademicDegree[]>('http://localhost:3000/academic-degrees', {
+        headers: head_obj,
+      })
       .pipe(
         catchError((error) => {
-            console.log('Veja o erro completo', error)
-            return throwError(() => new Error('Não foi possível encontrar os Estados Civis já cadastrados.'));
-          
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () =>
+              new Error(
+                'Não foi possível encontrar os Estados Civis já cadastrados.',
+              ),
+          )
+        }),
+      )
   }
 
-  createAcademicDegree(createAcademicDegreeData:ICreateAcademicDegreeDto): Observable<IAcademicDegree>{
-    const token = localStorage.getItem('access_token');
-    let head_obj = new HttpHeaders().set("Authorization", "bearer " + token);
-    return this.http.post<IAcademicDegree>('http://localhost:3000/academic-degrees', createAcademicDegreeData, { headers: head_obj })
+  createAcademicDegree(
+    createAcademicDegreeData: ICreateAcademicDegreeDto,
+  ): Observable<IAcademicDegree> {
+    const token = localStorage.getItem('access_token')
+    let head_obj = new HttpHeaders().set('Authorization', 'bearer ' + token)
+    return this.http
+      .post<IAcademicDegree>(
+        'http://localhost:3000/academic-degrees',
+        createAcademicDegreeData,
+        {headers: head_obj},
+      )
       .pipe(
         catchError((error) => {
-            console.log('Veja o erro completo', error)
-            return throwError(() => new Error('Não foi possível criar o estado civil.'));
-          
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () => new Error('Não foi possível criar o estado civil.'),
+          )
+        }),
+      )
   }
 
-  editAcademicDegree(editAcademicDegreeData:IUpdateAcademicDegree): Observable<IAcademicDegree>{
-    const token = localStorage.getItem('access_token');
-    let head_obj = new HttpHeaders().set("Authorization", "bearer " + token);
-    return this.http.put<IAcademicDegree>('http://localhost:3000/academic-degrees', editAcademicDegreeData, { headers: head_obj })
+  editAcademicDegree(
+    editAcademicDegreeData: IUpdateAcademicDegree,
+  ): Observable<IAcademicDegree> {
+    const token = localStorage.getItem('access_token')
+    let head_obj = new HttpHeaders().set('Authorization', 'bearer ' + token)
+    return this.http
+      .put<IAcademicDegree>(
+        'http://localhost:3000/academic-degrees',
+        editAcademicDegreeData,
+        {headers: head_obj},
+      )
       .pipe(
         catchError((error) => {
-            console.log('Veja o erro completo', error)
-            return throwError(() => new Error('Não foi possível criar o estado civil.'));
-          
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () => new Error('Não foi possível criar o estado civil.'),
+          )
+        }),
+      )
   }
 
+  deleteAcademicDegree(id: number): Observable<IAcademicDegree> {
+    const token = localStorage.getItem('access_token')
+    let head_obj = new HttpHeaders().set('Authorization', 'bearer ' + token)
+    return this.http
+      .delete<IAcademicDegree>('http://localhost:3000/academic-degrees/' + id, {
+        headers: head_obj,
+      })
+      .pipe(
+        catchError((error) => {
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () => new Error('Não foi possível criar o estado civil.'),
+          )
+        }),
+      )
+  }
 }
