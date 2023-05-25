97b0957b581b6ea2fbc6b42306e2f346499e5c98
F4289001 Hugo Sant Ana Guimaraes <hugo.santana@bb.com.br>
2023-05-25 18:23:04 -0300

    fix padroniza student-academic-formations

--- frontend/src/app/components/records/student-academic-formations/student-academic-formations.service.ts
+++ frontend/src/app/components/records/student-academic-formations/student-academic-formations.service.ts
@@ -1,71 +1,116 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { IStAcademicFormation, IStCreateAcademicFormation, IStUpdateAcademicFormation } from './types';
-
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Injectable} from '@angular/core'
+import {Router} from '@angular/router'
+import {Observable, throwError} from 'rxjs'
+import {catchError} from 'rxjs/operators'
+import {
+  IStAcademicFormation,
+  IStCreateAcademicFormation,
+  IStUpdateAcademicFormation,
+} from './types'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StudentAcademicFormationsService {
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) { };
-
-  findAllAcademicFormations(): Observable<IStAcademicFormation[]>{
-    const token = localStorage.getItem('access_token');
-    let head_obj = new HttpHeaders().set("Authorization", "bearer " + token);
-    return this.http.get<IStAcademicFormation[]>('http://localhost:3000/academic-formations', { headers: head_obj })
+  findAllAcademicFormations(): Observable<IStAcademicFormation[]> {
+    const token = localStorage.getItem('access_token')
+    let head_obj = new HttpHeaders().set('Authorization', 'bearer ' + token)
+    return this.http
+      .get<IStAcademicFormation[]>(
+        'http://localhost:3000/academic-formations',
+        {headers: head_obj},
+      )
       .pipe(
         catchError((error) => {
-            console.log('Veja o erro completo', error)
-            return throwError(() => new Error('Não foi possível encontrar as formações.'));
-          
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () => new Error('Não foi possível encontrar as formações.'),
+          )
+        }),
+      )
   }
 
-  findAllStudentAcademicFormations(): Observable<IStAcademicFormation[]>{
-    const token = localStorage.getItem('access_token');
-    let head_obj = new HttpHeaders().set("Authorization", "bearer " + token);
-    return this.http.get<IStAcademicFormation[]>('http://localhost:3000/academic-formations/student', { headers: head_obj })
+  findAllStudentAcademicFormations(): Observable<IStAcademicFormation[]> {
+    const token = localStorage.getItem('access_token')
+    let head_obj = new HttpHeaders().set('Authorization', 'bearer ' + token)
+    return this.http
+      .get<IStAcademicFormation[]>(
+        'http://localhost:3000/academic-formations/student',
+        {headers: head_obj},
+      )
       .pipe(
         catchError((error) => {
-            console.log('Veja o erro completo', error)
-            return throwError(() => new Error('Não foi possível encontrar as formações acadêmicas.'));
-          
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () =>
+              new Error('Não foi possível encontrar as formações acadêmicas.'),
+          )
+        }),
+      )
   }
 
-  createStudentAcademicFormation(createAcademicFormationData:IStCreateAcademicFormation): Observable<IStAcademicFormation>{
-    const token = localStorage.getItem('access_token');
-    let head_obj = new HttpHeaders().set("Authorization", "bearer " + token);
-    return this.http.post<IStAcademicFormation>('http://localhost:3000/academic-formations/student', createAcademicFormationData, { headers: head_obj })
+  createStudentAcademicFormation(
+    createAcademicFormationData: IStCreateAcademicFormation,
+  ): Observable<IStAcademicFormation> {
+    const token = localStorage.getItem('access_token')
+    let head_obj = new HttpHeaders().set('Authorization', 'bearer ' + token)
+    return this.http
+      .post<IStAcademicFormation>(
+        'http://localhost:3000/academic-formations/student',
+        createAcademicFormationData,
+        {headers: head_obj},
+      )
       .pipe(
         catchError((error) => {
-            console.log('Veja o erro completo', error)
-            return throwError(() => new Error('Não foi possível criar a formação acadêmica.'));
-          
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () => new Error('Não foi possível criar a formação acadêmica.'),
+          )
+        }),
+      )
   }
 
-  editStudentAcademicFormation(editAcademicFormationData:IStUpdateAcademicFormation): Observable<IStAcademicFormation>{
-    const token = localStorage.getItem('access_token');
-    let head_obj = new HttpHeaders().set("Authorization", "bearer " + token);
-    return this.http.put<IStAcademicFormation>('http://localhost:3000/academic-formations', editAcademicFormationData, { headers: head_obj })
+  editStudentAcademicFormation(
+    editAcademicFormationData: IStUpdateAcademicFormation,
+  ): Observable<IStAcademicFormation> {
+    const token = localStorage.getItem('access_token')
+    let head_obj = new HttpHeaders().set('Authorization', 'bearer ' + token)
+    return this.http
+      .put<IStAcademicFormation>(
+        'http://localhost:3000/academic-formations',
+        editAcademicFormationData,
+        {headers: head_obj},
+      )
       .pipe(
         catchError((error) => {
-            console.log('Veja o erro completo', error)
-            return throwError(() => new Error('Não foi possível criar o estado civil.'));
-          
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () => new Error('Não foi possível criar o estado civil.'),
+          )
+        }),
+      )
   }
 
+  deleteRegistry(registryId: number): Observable<string> {
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`)
+    return this.http
+      .delete<string>(
+        `http://localhost:3000/academic-formations/${registryId}`,
+        {
+          headers,
+        },
+      )
+      .pipe(
+        catchError((error) => {
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () => new Error('Não foi possível deletar o registro.'),
+          )
+        }),
+      )
+  }
 }
