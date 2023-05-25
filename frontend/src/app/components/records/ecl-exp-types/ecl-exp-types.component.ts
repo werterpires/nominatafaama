18cddd11770600562aa6a3ba5257845c685b65e4
F4289001 Hugo Sant Ana Guimaraes <hugo.santana@bb.com.br>
2023-05-25 16:26:44 -0300

    fix padroniza ecl-exp-types

--- frontend/src/app/components/records/ecl-exp-types/ecl-exp-types.component.ts
+++ frontend/src/app/components/records/ecl-exp-types/ecl-exp-types.component.ts
@@ -1,139 +1,165 @@
-import { Component, Input, Renderer2 } from '@angular/core';
-import { EclExpTypesService } from './ecl-exp-types.service';
-import { IPermissions } from '../../shared/container/types';
-import { IEclExpType, ICreateEclExpTypeDto, IUpdateEclExpType } from './types';
+import {Component, Input, Renderer2} from '@angular/core'
+import {EclExpTypesService} from './ecl-exp-types.service'
+import {IPermissions} from '../../shared/container/types'
+import {IEclExpType, ICreateEclExpTypeDto, IUpdateEclExpType} from './types'
 
 @Component({
   selector: 'app-ecl-exp-types',
   templateUrl: './ecl-exp-types.component.html',
-  styleUrls: ['./ecl-exp-types.component.css']
+  styleUrls: ['./ecl-exp-types.component.css'],
 })
 export class EclExpTypesComponent {
   constructor(
     private eclExpTypesService: EclExpTypesService,
-    private renderer: Renderer2){}
+    private renderer: Renderer2,
+  ) {}
 
-  @Input() permissions!: IPermissions;
-  allEclExpTypes: IEclExpType[] = [];
-  creatingEclExpType: boolean = false;
-  editingEclExpType: boolean = false;
+  @Input() permissions!: IPermissions
+  allEclExpTypes: IEclExpType[] = []
+  creatingEclExpType: boolean = false
+  editingEclExpType: boolean = false
   createEclExpTypeData: ICreateEclExpTypeDto = {
-   ecl_exp_type_name:""
-  };
-
-  isLoading: boolean = false;
-  done: boolean = false;
-  doneMessage: string = '';
-  error: boolean = false;
-  errorMessage: string = '';
-
-  shownBox: boolean = false;
-
-  ngOnInit(){
-    this.isLoading = true;
-    this.eclExpTypesService.findAllEclExpTypes().subscribe({
-      next: res => {
-        this.allEclExpTypes = res;
-        this.isLoading = false;
-      },
-      error: err => {
-        this.errorMessage = err.message;
-        this.error = true;
-        this.isLoading = false;
-      }
-    });
+    ecl_exp_type_name: '',
   }
 
-  showBox(){
-    const box = document.getElementById("boxHeadEclExpTypes");
-    const add = document.getElementById("eclExpTypeAddIcon");
-    const see = document.getElementById("seeMoreIconEclExpTypes");
-    this.shownBox = !this.shownBox;
-    if (this.shownBox){
-      box?.classList.replace("smallSectionBox", "sectionBox");
-      add?.classList.remove("hidden");
-      see?.classList.add("rotatedClock");
-      this.editingEclExpType = false;
-    }else{
-      box?.classList.replace("sectionBox", "smallSectionBox");
-      add?.classList.add("hidden");
-      see?.classList.remove("rotatedClock");
+  isLoading: boolean = false
+  done: boolean = false
+  doneMessage: string = ''
+  error: boolean = false
+  errorMessage: string = ''
+
+  shownBox: boolean = false
+
+  ngOnInit() {
+    this.isLoading = true
+    this.eclExpTypesService.findAllEclExpTypes().subscribe({
+      next: (res) => {
+        this.allEclExpTypes = res
+        this.isLoading = false
+      },
+      error: (err) => {
+        this.errorMessage = err.message
+        this.error = true
+        this.isLoading = false
+      },
+    })
+  }
+
+  showBox() {
+    const box = document.getElementById('boxHeadEclExpTypes')
+    const add = document.getElementById('eclExpTypeAddIcon')
+    const see = document.getElementById('seeMoreIconEclExpTypes')
+    this.shownBox = !this.shownBox
+    if (this.shownBox) {
+      box?.classList.replace('smallSectionBox', 'sectionBox')
+      add?.classList.remove('hidden')
+      see?.classList.add('rotatedClock')
+      this.editingEclExpType = false
+    } else {
+      box?.classList.replace('sectionBox', 'smallSectionBox')
+      add?.classList.add('hidden')
+      see?.classList.remove('rotatedClock')
     }
   }
 
-  createForm(){
-    this.creatingEclExpType = true;
+  createForm() {
+    this.creatingEclExpType = true
   }
 
-  createEclExpType(){
-    this.isLoading = true;
-    this.eclExpTypesService.createEclExpType(this.createEclExpTypeData).subscribe({
-      next: res => {
-        this.doneMessage = 'Tipo de experiência ECL criado com sucesso.';
-        this.done = true;
-        this.isLoading = false;
-        this.ngOnInit();
-        this.createEclExpTypeData.ecl_exp_type_name = "";
-        this.creatingEclExpType = false;
-      },
-      error: err => {
-        this.errorMessage = err.message;
-        this.error = true;
-        this.isLoading = false;
-      }
-    });
+  createEclExpType() {
+    this.isLoading = true
+    this.eclExpTypesService
+      .createEclExpType(this.createEclExpTypeData)
+      .subscribe({
+        next: (res) => {
+          this.doneMessage = 'Tipo de experiência ECL criado com sucesso.'
+          this.done = true
+          this.isLoading = false
+          this.ngOnInit()
+          this.createEclExpTypeData.ecl_exp_type_name = ''
+          this.creatingEclExpType = false
+        },
+        error: (err) => {
+          this.errorMessage = err.message
+          this.error = true
+          this.isLoading = false
+        },
+      })
   }
 
   changeTagType(paragraphId: string, buttonId: string, inputId: string) {
-    const paragraph = document.getElementById(paragraphId);
-    const input = document.getElementById(inputId) as HTMLInputElement;
+    const paragraph = document.getElementById(paragraphId)
+    const input = document.getElementById(inputId) as HTMLInputElement
 
     if (paragraph !== null && paragraph.textContent && input !== null) {
-      input.classList.remove('hidden');
-      paragraph.classList.add('hidden');
+      input.classList.remove('hidden')
+      paragraph.classList.add('hidden')
 
-      input.value = paragraph.textContent;     
-      input.oninput = function(){
-        const button = document.getElementById(buttonId)?.classList.remove('hidden');
-      };
+      input.value = paragraph.textContent
+      input.oninput = function () {
+        const button = document
+          .getElementById(buttonId)
+          ?.classList.remove('hidden')
+      }
 
-      input.focus();
+      input.focus()
 
-      input.onblur = function() {
-        paragraph.textContent = input.value;
-        input.classList.add('hidden');
-        paragraph.classList.remove('hidden');
-      };
-    }  
+      input.onblur = function () {
+        paragraph.textContent = input.value
+        input.classList.add('hidden')
+        paragraph.classList.remove('hidden')
+      }
+    }
   }
 
-  editEclExpType(i: number, buttonId: string){
-    this.isLoading = true;
+  editEclExpType(i: number, buttonId: string) {
+    this.isLoading = true
     const editEclExpTypeData: IUpdateEclExpType = {
       ecl_exp_type_id: this.allEclExpTypes[i].ecl_exp_type_id,
-      ecl_exp_type_name: this.allEclExpTypes[i].ecl_exp_type_name
-    };
+      ecl_exp_type_name: this.allEclExpTypes[i].ecl_exp_type_name,
+    }
 
     this.eclExpTypesService.editEclExpType(editEclExpTypeData).subscribe({
-      next: res => {
-        this.doneMessage = 'Tipo de experiência ECL editado com sucesso.';
-        this.done = true;
-        const button = document.getElementById(buttonId)?.classList.add('hidden');
-        this.isLoading = false;
+      next: (res) => {
+        this.doneMessage = 'Tipo de experiência ECL editado com sucesso.'
+        this.done = true
+        const button = document
+          .getElementById(buttonId)
+          ?.classList.add('hidden')
+        this.isLoading = false
       },
-      error: err => {
-        this.errorMessage = err.message;
-        this.error = true;
-        this.isLoading = false;
-      }
-    });
+      error: (err) => {
+        this.errorMessage = err.message
+        this.error = true
+        this.isLoading = false
+      },
+    })
   }
 
-  closeError(){
-    this.error = false;
+  deleteAssociation(i: number) {
+    this.isLoading = true
+    const associationId = this.allEclExpTypes[i].ecl_exp_type_id
+
+    this.eclExpTypesService.deleteEclExpType(associationId).subscribe({
+      next: (res) => {
+        this.doneMessage = 'Associação deletada com sucesso.'
+        this.done = true
+        this.isLoading = false
+        this.ngOnInit()
+      },
+      error: (err) => {
+        this.errorMessage = 'Não foi possível deletar a associação.'
+        this.error = true
+        this.isLoading = false
+      },
+    })
   }
 
-  closeDone(){
-    this.done = false;
+  closeError() {
+    this.error = false
+  }
+
+  closeDone() {
+    this.done = false
   }
 }
--- frontend/src/app/components/records/ecl-exp-types/ecl-exp-types.service.ts
+++ frontend/src/app/components/records/ecl-exp-types/ecl-exp-types.service.ts
@@ -1,53 +1,96 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { ICreateEclExpTypeDto, IEclExpType, IUpdateEclExpType } from './types';
+import {Injectable} from '@angular/core'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
+import {Observable, throwError} from 'rxjs'
+import {catchError} from 'rxjs/operators'
+import {ICreateEclExpTypeDto, IEclExpType, IUpdateEclExpType} from './types'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EclExpTypesService {
-
   constructor(private http: HttpClient) {}
 
   findAllEclExpTypes(): Observable<IEclExpType[]> {
-    const token = localStorage.getItem('access_token');
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`)
 
-    return this.http.get<IEclExpType[]>(`http://localhost:3000/ecl-exp-types`, { headers })
+    return this.http
+      .get<IEclExpType[]>(`http://localhost:3000/ecl-exp-types`, {headers})
       .pipe(
         catchError((error) => {
-          console.log('Veja o erro completo', error);
-          return throwError(() => new Error('Não foi possível encontrar os tipos de experiência em ECL.'));
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () =>
+              new Error(
+                'Não foi possível encontrar os tipos de experiência em ECL.',
+              ),
+          )
+        }),
+      )
   }
 
-  createEclExpType(createEclExpTypeData: ICreateEclExpTypeDto): Observable<IEclExpType> {
-    const token = localStorage.getItem('access_token');
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  createEclExpType(
+    createEclExpTypeData: ICreateEclExpTypeDto,
+  ): Observable<IEclExpType> {
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`)
 
-    return this.http.post<IEclExpType>(`http://localhost:3000/ecl-exp-types`, createEclExpTypeData, { headers })
+    return this.http
+      .post<IEclExpType>(
+        `http://localhost:3000/ecl-exp-types`,
+        createEclExpTypeData,
+        {headers},
+      )
       .pipe(
         catchError((error) => {
-          console.log('Veja o erro completo', error);
-          return throwError(() => new Error('Não foi possível criar o tipo de experiência de educação cristã.'));
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () =>
+              new Error(
+                'Não foi possível criar o tipo de experiência de educação cristã.',
+              ),
+          )
+        }),
+      )
   }
 
-  editEclExpType(editEclExpTypeData: IUpdateEclExpType): Observable<IEclExpType> {
-    const token = localStorage.getItem('access_token');
-    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  editEclExpType(
+    editEclExpTypeData: IUpdateEclExpType,
+  ): Observable<IEclExpType> {
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`)
 
-    return this.http.put<IEclExpType>(`http://localhost:3000/ecl-exp-types`, editEclExpTypeData, { headers })
+    return this.http
+      .put<IEclExpType>(
+        `http://localhost:3000/ecl-exp-types`,
+        editEclExpTypeData,
+        {headers},
+      )
       .pipe(
         catchError((error) => {
-          console.log('Veja o erro completo', error);
-          return throwError(() => new Error('Não foi possível atualizar o tipo de experiência de educação cristã.'));
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(
+            () =>
+              new Error(
+                'Não foi possível atualizar o tipo de experiência de educação cristã.',
+              ),
+          )
+        }),
+      )
   }
-  
+
+  deleteEclExpType(eclExpTypeId: number): Observable<string> {
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`)
+    return this.http
+      .delete<string>(`http://localhost:3000/ecl-exp-types/${eclExpTypeId}`, {
+        headers,
+      })
+      .pipe(
+        catchError((error) => {
+          console.log('Veja o erro completo', error)
+          return throwError(() => new Error('Não foi possível deletar.'))
+        }),
+      )
+  }
 }
