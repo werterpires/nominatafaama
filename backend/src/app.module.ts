a6aa1720dd564fee2df63fef28a11f42970f9408
F4289001 Hugo Sant Ana Guimaraes <hugo.santana@bb.com.br>
2023-05-26 10:33:17 -0300

    fix cast no knex de tiny para boolean

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { KnexModule } from 'nest-knexjs';
-import { config } from 'dotenv';
-import { UsersModule } from './modules/users/users.module';
-import { PeopleModule } from './modules/people/people.module';
-import { AuthModule } from './shared/auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
-import { JwtAuthGuard } from './shared/auth/guards/jwt-auth.guard';
-import { RolesModule } from './shared/roles/roles.module';
-import { RolesGuard } from './shared/roles/gz_guards/roles.guard';
-import { ResourcesModule } from './modules/resources/resources.module';
-import { StudentsModule } from './modules/students/students.module';
-import { SpousesModule } from './modules/spouses/spouses.module';
-import { InfoModule } from './modules/info/info.module';
+import { Module } from '@nestjs/common'
+import { KnexModule } from 'nest-knexjs'
+import { config } from 'dotenv'
+import { UsersModule } from './modules/users/users.module'
+import { PeopleModule } from './modules/people/people.module'
+import { AuthModule } from './shared/auth/auth.module'
+import { APP_GUARD } from '@nestjs/core'
+import { JwtAuthGuard } from './shared/auth/guards/jwt-auth.guard'
+import { RolesModule } from './shared/roles/roles.module'
+import { RolesGuard } from './shared/roles/gz_guards/roles.guard'
+import { ResourcesModule } from './modules/resources/resources.module'
+import { StudentsModule } from './modules/students/students.module'
+import { SpousesModule } from './modules/spouses/spouses.module'
+import { InfoModule } from './modules/info/info.module'
 
-config();
+config()
 
 @Module({
   imports: [
@@ -26,6 +26,12 @@ config();
           user: process.env.MYSQL_USER,
           password: process.env.MYSQL_PASSWORD,
           database: process.env.MYSQL_DATABASE,
+          typeCast: function (field, next) {
+            if (field.type === 'TINY' && field.length === 1) {
+              return field.string() === '1' // 1 = true, 0 = false
+            }
+            return next()
+          },
         },
       },
     }),
