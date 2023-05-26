cd56aad1a7efbb270c92b4a63d16efb72cd548c8
Werter Pires <werterpires23@hotmail.com>
2023-05-26 00:19:41 -0300

    fix delete on several resources

--- backend/src/modules/resources/ecl-exp-types/model/ecl-exp-types.model.ts
+++ backend/src/modules/resources/ecl-exp-types/model/ecl-exp-types.model.ts
@@ -1,60 +1,62 @@
-import { Injectable } from '@nestjs/common';
-import { Knex } from 'knex';
-import { InjectModel } from 'nest-knexjs';
-import { IEclExpType, ICreateEclExpType, IUpdateEclExpType } from '../types/types';
+import {Injectable} from '@nestjs/common'
+import {Knex} from 'knex'
+import {InjectModel} from 'nest-knexjs'
+import {IEclExpType, ICreateEclExpType, IUpdateEclExpType} from '../types/types'
 
 @Injectable()
 export class EclExpTypesModel {
   constructor(@InjectModel() private readonly knex: Knex) {}
 
-  async createEclExpType({ ecl_exp_type_name }: ICreateEclExpType): Promise<IEclExpType> {
-    let eclExpType: IEclExpType | null = null;
-    let sentError: Error | null = null;
+  async createEclExpType({
+    ecl_exp_type_name,
+  }: ICreateEclExpType): Promise<IEclExpType> {
+    let eclExpType: IEclExpType | null = null
+    let sentError: Error | null = null
 
     await this.knex.transaction(async (trx) => {
       try {
         const [result] = await trx('ecl_exp_types').insert({
           ecl_exp_type_name,
-        });
+        })
 
         eclExpType = {
           ecl_exp_type_id: result,
           ecl_exp_type_name,
           created_at: new Date(),
           updated_at: new Date(),
-        };
+        }
 
-        await trx.commit();
+        await trx.commit()
       } catch (error) {
-        await trx.rollback();
+        await trx.rollback()
         if (error.code === 'ER_DUP_ENTRY') {
-          sentError = new Error('EclExpType already exists');
+          sentError = new Error('EclExpType already exists')
         } else {
-          sentError = new Error(error.sqlMessage);
+          sentError = new Error(error.sqlMessage)
         }
       }
-    });
+    })
 
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
 
-    return eclExpType!;
+    return eclExpType!
   }
 
   async findEclExpTypeById(id: number): Promise<IEclExpType | null> {
-    let eclExpType: IEclExpType | null = null;
-    let sentError: Error | null = null;
+    let eclExpType: IEclExpType | null = null
+    let sentError: Error | null = null
 
     await this.knex.transaction(async (trx) => {
       try {
         const result = await trx
           .table('ecl_exp_types')
           .select('*')
-          .where('ecl_exp_type_id', '=', id);
+          .where('ecl_exp_type_id', '=', id)
 
         if (result.length < 1) {
-          throw new Error('EclExpType not found');
+          throw new Error('EclExpType not found')
         }
 
         eclExpType = {
@@ -62,105 +64,114 @@ export class EclExpTypesModel {
           ecl_exp_type_name: result[0].ecl_exp_type_name,
           created_at: result[0].created_at,
           updated_at: result[0].updated_at,
-        };
+        }
 
-        await trx.commit();
+        await trx.commit()
       } catch (error) {
-        sentError = new Error(error.message);
-        await trx.rollback();
-        throw error;
+        sentError = new Error(error.message)
+        await trx.rollback()
+        throw error
       }
-    });
+    })
 
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
 
-    return eclExpType;
+    return eclExpType
   }
 
   async findAllEclExpTypes(): Promise<IEclExpType[]> {
-    let eclExpTypesList: IEclExpType[] = [];
-    let sentError: Error | null = null;
+    let eclExpTypesList: IEclExpType[] = []
+    let sentError: Error | null = null
 
     await this.knex.transaction(async (trx) => {
       try {
-        const results = await trx.table('ecl_exp_types').select('*');
+        const results = await trx.table('ecl_exp_types').select('*')
 
         eclExpTypesList = results.map((row: any) => ({
           ecl_exp_type_id: row.ecl_exp_type_id,
           ecl_exp_type_name: row.ecl_exp_type_name,
           created_at: row.created_at,
           updated_at: row.updated_at,
-        }));
+        }))
 
-        await trx.commit();
+        await trx.commit()
       } catch (error) {
-        await trx.rollback();
-        sentError = new Error(error.sqlMessage);
+        await trx.rollback()
+        sentError = new Error(error.sqlMessage)
       }
-    });
+    })
 
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
 
-    return eclExpTypesList;
+    return eclExpTypesList
   }
 
-  async updateEclExpTypeById(updateEclExpType: IUpdateEclExpType): Promise<IEclExpType> {
-    let updatedEclExpType: IEclExpType | null = null;
-    let sentError: Error | null = null;
+  async updateEclExpTypeById(
+    updateEclExpType: IUpdateEclExpType,
+  ): Promise<IEclExpType> {
+    let updatedEclExpType: IEclExpType | null = null
+    let sentError: Error | null = null
 
     await this.knex.transaction(async (trx) => {
       try {
-        const { ecl_exp_type_name } = updateEclExpType;
-        const { ecl_exp_type_id } = updateEclExpType;
+        const {ecl_exp_type_name} = updateEclExpType
+        const {ecl_exp_type_id} = updateEclExpType
 
         await trx('ecl_exp_types')
           .where('ecl_exp_type_id', ecl_exp_type_id)
-          .update({ ecl_exp_type_name });
+          .update({ecl_exp_type_name})
 
-        updatedEclExpType = await this.findEclExpTypeById(ecl_exp_type_id);
+        updatedEclExpType = await this.findEclExpTypeById(ecl_exp_type_id)
 
-        await trx.commit();
+        await trx.commit()
       } catch (error) {
-        await trx.rollback();
-        sentError = new Error(error.message);
+        await trx.rollback()
+        sentError = new Error(error.message)
       }
-    });
+    })
 
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
 
     if (updatedEclExpType === null) {
-      throw new Error('Failed to update EclExpType.');
+      throw new Error('Failed to update EclExpType.')
     }
 
-    return updatedEclExpType;
+    return updatedEclExpType
   }
 
   async deleteEclExpTypeById(id: number): Promise<string> {
-    let sentError: Error | null = null;
-    let message: string = '';
+    let sentError: Error | null = null
+    let message: string = ''
 
     await this.knex.transaction(async (trx) => {
       try {
-        await trx('ecl_exp_types').where('ecl_exp_type_id', id).del();
+        await trx('ecl_exp_types').where('ecl_exp_type_id', id).del()
 
-        await trx.commit();
+        await trx.commit()
       } catch (error) {
-        sentError = new Error(error.message);
-        await trx.rollback();
+        if (error.code == 'ER_ROW_IS_REFERENCED_2') {
+          sentError = new Error(
+            'Alguém está usando o tipo de experiência eclesiástica que você está tentando apagar.',
+          )
+        } else {
+          sentError = new Error(error.message)
+        }
+
+        await trx.rollback()
       }
-    });
+    })
 
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
 
-    message = 'EclExpType deleted successfully.';
-    return message;
+    message = 'EclExpType deleted successfully.'
+    return message
   }
 }
--- backend/src/modules/resources/hiring-status/model/hiring-status.model.ts
+++ backend/src/modules/resources/hiring-status/model/hiring-status.model.ts
@@ -1,170 +1,181 @@
-import { Injectable } from '@nestjs/common';
-import { Knex } from 'knex';
-import { InjectModel } from 'nest-knexjs';
-import { ICreateHiringStatus, IHiringStatus, IUpdateHiringStatus } from '../types/types';
+import {Injectable} from '@nestjs/common'
+import {Knex} from 'knex'
+import {InjectModel} from 'nest-knexjs'
+import {
+  ICreateHiringStatus,
+  IHiringStatus,
+  IUpdateHiringStatus,
+} from '../types/types'
 
 @Injectable()
 export class HiringStatusModel {
   constructor(@InjectModel() private readonly knex: Knex) {}
 
-  async createHiringStatus({ hiring_status_name, hiring_status_description }: ICreateHiringStatus): Promise<IHiringStatus> {
-    let hiringStatus: IHiringStatus | null = null;
-    let sentError: Error | null = null;
-  
+  async createHiringStatus({
+    hiring_status_name,
+    hiring_status_description,
+  }: ICreateHiringStatus): Promise<IHiringStatus> {
+    let hiringStatus: IHiringStatus | null = null
+    let sentError: Error | null = null
+
     await this.knex.transaction(async (trx) => {
       try {
         const [result] = await trx('hiring_status').insert({
           hiring_status_name,
-          hiring_status_description
-        });
-  
+          hiring_status_description,
+        })
+
         hiringStatus = {
           hiring_status_id: result,
           hiring_status_name,
           hiring_status_description,
           created_at: new Date(),
           updated_at: new Date(),
-        };
-  
-        await trx.commit();
+        }
+
+        await trx.commit()
       } catch (error) {
-        await trx.rollback();
+        await trx.rollback()
         if (error.code === 'ER_DUP_ENTRY') {
-          sentError = new Error('Status de contratação já existente.');
+          sentError = new Error('Status de contratação já existente.')
         } else {
-          sentError = new Error(error.sqlMessage);
+          sentError = new Error(error.sqlMessage)
         }
       }
-    });
-  
+    })
+
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
-  
-    return hiringStatus!;
+
+    return hiringStatus!
   }
-  
+
   async findHiringStatusById(id: number): Promise<IHiringStatus | null> {
-    let hiringStatus: IHiringStatus | null = null;
-    let sentError: Error | null = null;
-  
+    let hiringStatus: IHiringStatus | null = null
+    let sentError: Error | null = null
+
     await this.knex.transaction(async (trx) => {
       try {
         const result = await trx
           .table('hiring_status')
           .select('*')
-          .where('hiring_status_id', '=', id);
-  
+          .where('hiring_status_id', '=', id)
+
         if (result.length < 1) {
-          throw new Error('Status de contratação não encontrado.');
+          throw new Error('Status de contratação não encontrado.')
         }
-  
+
         hiringStatus = {
           hiring_status_id: result[0].hiring_status_id,
           hiring_status_name: result[0].hiring_status_name,
           hiring_status_description: result[0].hiring_status_description,
           created_at: result[0].created_at,
           updated_at: result[0].updated_at,
-        };
-  
-        await trx.commit();
+        }
+
+        await trx.commit()
       } catch (error) {
-        sentError = new Error(error.message);
-        await trx.rollback();
-        throw error;
+        sentError = new Error(error.message)
+        await trx.rollback()
+        throw error
       }
-    });
-  
+    })
+
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
-  
-    return hiringStatus;
+
+    return hiringStatus
   }
-  
+
   async findAllHiringStatus(): Promise<IHiringStatus[]> {
-    let hiringStatusList: IHiringStatus[] = [];
-    let sentError: Error | null = null;
-  
+    let hiringStatusList: IHiringStatus[] = []
+    let sentError: Error | null = null
+
     await this.knex.transaction(async (trx) => {
       try {
-        const results = await trx.table('hiring_status').select('*');
-  
+        const results = await trx.table('hiring_status').select('*')
+
         hiringStatusList = results.map((row: any) => ({
           hiring_status_id: row.hiring_status_id,
           hiring_status_name: row.hiring_status_name,
           hiring_status_description: row.hiring_status_description,
           created_at: row.created_at,
           updated_at: row.updated_at,
-        }));
-  
-        await trx.commit();
+        }))
+
+        await trx.commit()
       } catch (error) {
-        await trx.rollback();
-        sentError = new Error(error.sqlMessage);
+        await trx.rollback()
+        sentError = new Error(error.sqlMessage)
       }
-    });
-  
+    })
+
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
-  
-    return hiringStatusList;
+
+    return hiringStatusList
   }
 
-  async updateHiringStatusById(updateHiringStatus: IUpdateHiringStatus): Promise<IHiringStatus> {
-    let updatedHirignStatus: IHiringStatus | null = null;
-    let sentError: Error | null = null;
-  
+  async updateHiringStatusById(
+    updateHiringStatus: IUpdateHiringStatus,
+  ): Promise<IHiringStatus> {
+    let updatedHirignStatus: IHiringStatus | null = null
+    let sentError: Error | null = null
+
     await this.knex.transaction(async (trx) => {
       try {
-        const { hiring_status_name, hiring_status_description, hiring_status_id } = updateHiringStatus;
-  
+        const {
+          hiring_status_name,
+          hiring_status_description,
+          hiring_status_id,
+        } = updateHiringStatus
+
         await trx('hiring_status')
           .where('hiring_status_id', hiring_status_id)
-          .update({hiring_status_name, hiring_status_description });
-  
-        updatedHirignStatus = await this.findHiringStatusById(hiring_status_id);
-  
-        await trx.commit();
+          .update({hiring_status_name, hiring_status_description})
+
+        updatedHirignStatus = await this.findHiringStatusById(hiring_status_id)
+
+        await trx.commit()
       } catch (error) {
-        await trx.rollback();
-        sentError = new Error(error.message);
+        await trx.rollback()
+        sentError = new Error(error.message)
       }
-    });
-  
+    })
+
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
-  
+
     if (updatedHirignStatus === null) {
-      throw new Error('Falha ao atualizar status de contratação.');
+      throw new Error('Falha ao atualizar status de contratação.')
     }
-  
-    return updatedHirignStatus;
+
+    return updatedHirignStatus
   }
-  
+
   async deleteHiringStatusById(id: number): Promise<string> {
-    let sentError: Error | null = null;
-    let message: string = '';
-  
+    let sentError: Error | null = null
+    let message: string = ''
+
     await this.knex.transaction(async (trx) => {
       try {
-        await trx('hiring_status').where('hiring_status_id', id).del();
-        await trx.commit();
+        await trx('hiring_status').where('hiring_status_id', id).del()
+        await trx.commit()
       } catch (error) {
-        sentError = new Error(error.message);
-        await trx.rollback();
+        sentError = new Error(error.message)
+        await trx.rollback()
       }
-    });
-  
+    })
+
     if (sentError) {
-      throw sentError;
+      throw sentError
     }
-  
-    message = 'Union deleted successfully.';
-    return message;
+
+    message = 'Union deleted successfully.'
+    return message
   }
-  
-  
 }
