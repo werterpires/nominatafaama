09640edebeab8ac73b2ddb47bb521d77712b6680
F4289001 Hugo Sant Ana Guimaraes <hugo.santana@bb.com.br>
2023-05-28 09:31:26 -0300

    fix estilo de edição na edição de registros

--- frontend/src/app/components/shared/shared.service.ts/login.service.ts
+++ frontend/src/app/components/shared/shared.service.ts/login.service.ts
@@ -1,11 +1,11 @@
-import {HttpClient} from '@angular/common/http'
-import {Injectable} from '@angular/core'
-import {Router} from '@angular/router'
-import {BehaviorSubject, catchError, tap, throwError} from 'rxjs'
-import {ILoginDto} from '../../login/login.Dto'
-import {IUserApproved} from '../container/types'
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Router } from '@angular/router'
+import { BehaviorSubject, catchError, tap, throwError } from 'rxjs'
+import { ILoginDto } from '../../login/login.Dto'
+import { IUserApproved } from '../container/types'
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class LoginService {
   private user = new BehaviorSubject<IUserApproved | null>(null)
   user$ = this.user.asObservable()
@@ -31,7 +31,7 @@ export class LoginService {
 
   login(loginData: ILoginDto) {
     return this.http
-      .post<{access_token: string}>('http://localhost:3000/login', loginData)
+      .post<{ access_token: string }>('http://localhost:3000/login', loginData)
       .pipe(
         catchError((error) => {
           if ((error.statusText = 'Unauthorized')) {
@@ -60,13 +60,21 @@ export class LoginService {
       localStorage.setItem('access_token', this.userToken)
       this.http
         .get<IUserApproved>('http://localhost:3000/users/roles', {
-          headers: {Authorization: 'bearer ' + token},
+          headers: { Authorization: 'bearer ' + token },
         })
-        .subscribe((userApproved) => {
-          if (userApproved.user_approved) {
-            this.user.next(userApproved)
-            this.router.navigateByUrl('/')
-          } else throw Error('User not approved')
+        .subscribe({
+          next: (userApproved) => {
+            if (userApproved.user_approved) {
+              this.user.next(userApproved)
+              this.router.navigateByUrl('/')
+            } else throw Error('User not approved')
+          },
+          error: (error: HttpErrorResponse) => {
+            console.error(error)
+            if (error.status === 401) {
+              this.logout()
+            }
+          },
         })
     }
   }
