453968175cd0bb1ac1258d450664669b501f2898
F4289001 Hugo Sant Ana Guimaraes <hugo.santana@bb.com.br>
2023-05-28 20:15:56 -0300

    fix padroniza publication-types

--- frontend/src/app/components/records/publication-types/publication-types.service.ts
+++ frontend/src/app/components/records/publication-types/publication-types.service.ts
@@ -1,12 +1,12 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Observable, throwError } from 'rxjs'
+import { catchError } from 'rxjs/operators'
 import {
   CreatePublicationTypeDto,
   IPublicationType,
   UpdatePublicationTypeDto,
-} from './types';
+} from './types'
 
 @Injectable({
   providedIn: 'root',
@@ -14,78 +14,79 @@ import {
 export class PublicationTypeService {
   constructor(private http: HttpClient) {}
 
-  findAllPublicationTypes(): Observable<IPublicationType[]> {
-    const token = localStorage.getItem('access_token');
-    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`);
+  findAllRegistries(): Observable<IPublicationType[]> {
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`)
     return this.http
       .get<IPublicationType[]>('http://localhost:3000/publication-types', {
         headers,
       })
       .pipe(
         catchError((error) => {
-          console.log('Veja o erro completo', error);
+          console.log('Veja o erro completo', error)
           return throwError(
-            () => new Error('Não foi possível encontrar as unions cadastradas.')
-          );
-        })
-      );
+            () =>
+              new Error('Não foi possível encontrar as unions cadastradas.'),
+          )
+        }),
+      )
   }
 
-  createPublicationType(
-    createPublicationTypeData: CreatePublicationTypeDto
+  createRegistry(
+    createPublicationTypeData: CreatePublicationTypeDto,
   ): Observable<IPublicationType> {
-    const token = localStorage.getItem('access_token');
-    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`);
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`)
     return this.http
       .post<IPublicationType>(
         'http://localhost:3000/publication-types',
         createPublicationTypeData,
-        { headers }
+        { headers },
       )
       .pipe(
         catchError((error) => {
-          console.log('Veja o erro completo', error);
-          return throwError(() => new Error('Não foi possível criar a union.'));
-        })
-      );
+          console.log('Veja o erro completo', error)
+          return throwError(() => new Error('Não foi possível criar a union.'))
+        }),
+      )
   }
 
-  updatePublicationType(
-    updatePublicationTypeData: UpdatePublicationTypeDto
+  updateRegistry(
+    updatePublicationTypeData: UpdatePublicationTypeDto,
   ): Observable<IPublicationType> {
-    const token = localStorage.getItem('access_token');
-    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`);
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`)
     return this.http
       .put<IPublicationType>(
         'http://localhost:3000/publication-types',
         updatePublicationTypeData,
-        { headers }
+        { headers },
       )
       .pipe(
         catchError((error) => {
-          console.log('Veja o erro completo', error);
+          console.log('Veja o erro completo', error)
           return throwError(
-            () => new Error('Não foi possível atualizar a union.')
-          );
-        })
-      );
+            () => new Error('Não foi possível atualizar a union.'),
+          )
+        }),
+      )
   }
 
-  deletePublicationType(publicationTypeId: number): Observable<string> {
-    const token = localStorage.getItem('access_token');
-    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`);
+  deleteRegistry(publicationTypeId: number): Observable<string> {
+    const token = localStorage.getItem('access_token')
+    const headers = new HttpHeaders().set('Authorization', `bearer ${token}`)
     return this.http
       .delete<string>(
         `http://localhost:3000/publication-types/${publicationTypeId}`,
-        { headers }
+        { headers },
       )
       .pipe(
         catchError((error) => {
-          console.log('Veja o erro completo', error);
+          console.log('Veja o erro completo', error)
           return throwError(
-            () => new Error('Não foi possível deletar a union.')
-          );
-        })
-      );
+            () => new Error('Não foi possível deletar a union.'),
+          )
+        }),
+      )
   }
 }
--- frontend/src/app/components/records/publication-types/types.ts
+++ frontend/src/app/components/records/publication-types/types.ts
@@ -1,20 +1,18 @@
 export interface IPublicationType {
-    publication_type_id: number;
-    publication_type: string;
-    instructions: string;
-  }
+  publication_type_id: number
+  publication_type: string
+  instructions: string
+  created_at: string
+  updated_at: string
+}
 
-  export interface CreatePublicationTypeDto {
-    publication_type: string;
-    instructions: string;
-  }
+export interface CreatePublicationTypeDto {
+  publication_type: string
+  instructions: string
+}
 
-  export interface UpdatePublicationTypeDto {
-    publication_type: string;
-    instructions: string;
-    publication_type_id: number;
-  }
-
-
-
-
+export interface UpdatePublicationTypeDto {
+  publication_type: string
+  instructions: string
+  publication_type_id: number
+}
