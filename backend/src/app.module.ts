1b4ec3fd14f6159bf29c1397f3ee16d674c0f8aa
Werter Pires <werterpires23@hotmail.com>
2023-05-29 22:33:05 -0300

    config env production

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { APP_GUARD } from '@nestjs/core'
 import { config } from 'dotenv'
-import { KnexModule } from 'nest-knexjs'
+import { KnexModule, KnexModuleOptions } from 'nest-knexjs'
 import { InfoModule } from './modules/info/info.module'
 import { PeopleModule } from './modules/people/people.module'
 import { ResourcesModule } from './modules/resources/resources.module'
@@ -12,34 +12,48 @@ import { AuthModule } from './shared/auth/auth.module'
 import { JwtAuthGuard } from './shared/auth/guards/jwt-auth.guard'
 import { RolesGuard } from './shared/roles/gz_guards/roles.guard'
 import { RolesModule } from './shared/roles/roles.module'
-import { StudentPhotosModule } from './components/modules/info/student-photos/student-photos.module';
 
 config()
 
+const devConfig: KnexModuleOptions = {
+  config: {
+    client: 'mssql',
+    useNullAsDefault: true,
+    connection: {
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      typeCast: function (field, next) {
+        if (field.type === 'TINY' && field.length === 1) {
+          return field.string() === '1' // 1 = true, 0 = false
+        } else if (field.type === 'DATE' && field.length > 1) {
+          return field.string() // 1 = true, 0 = false
+        } else if (field.type === 'DATETIME' && field.length > 1) {
+          return field.string().substring(0, 10) // 1 = true, 0 = false
+        }
+        return next()
+      },
+    },
+  },
+}
+
+const prodConfig: KnexModuleOptions = {
+  config: {
+    client: 'mssql',
+    connection: {
+      host: process.env.SQL_HOST,
+      user: process.env.SQL_USER,
+      password: process.env.SQL_PASS,
+      database: process.env.SQL_DB,
+      options: { encrypt: process.env.DEV_MODE ? false : true },
+    },
+  },
+}
+
 @Module({
   imports: [
-    KnexModule.forRoot({
-      config: {
-        client: 'mysql2',
-        useNullAsDefault: true,
-        connection: {
-          host: process.env.MYSQL_HOST,
-          user: process.env.MYSQL_USER,
-          password: process.env.MYSQL_PASSWORD,
-          database: process.env.MYSQL_DATABASE,
-          typeCast: function (field, next) {
-            if (field.type === 'TINY' && field.length === 1) {
-              return field.string() === '1' // 1 = true, 0 = false
-            } else if (field.type === 'DATE' && field.length > 1) {
-              return field.string() // 1 = true, 0 = false
-            } else if (field.type === 'DATETIME' && field.length > 1) {
-              return field.string().substring(0, 10) // 1 = true, 0 = false
-            }
-            return next()
-          },
-        },
-      },
-    }),
+    KnexModule.forRoot(process.env.DEV ? devConfig : prodConfig),
     UsersModule,
     PeopleModule,
     AuthModule,
@@ -48,7 +62,6 @@ config()
     StudentsModule,
     SpousesModule,
     InfoModule,
-    StudentPhotosModule,
   ],
   providers: [
     {
