235188fce7b4effda6d258eea298e2dd8bcde7e2
F4289001 Hugo Sant Ana Guimaraes <hugo.santana@bb.com.br>
2023-05-30 19:20:08 -0300

    fix porta do backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
       transform: true, // Faz a transformação dos params e query quando são numeros ou outras coisas
     }),
   )
-  await app.listen(3000)
+  await app.listen(process.env.PORT || 3000)
 }
 void bootstrap()
