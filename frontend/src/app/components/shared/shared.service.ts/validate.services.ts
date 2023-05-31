7b82e260845db277a9f22f30a086497f11a16df2
F4289001 Hugo Sant Ana Guimaraes <hugo.santana@bb.com.br>
2023-05-31 15:08:33 -0300

    add acentos na regex de nome no logon

--- frontend/src/app/components/shared/shared.service.ts/validate.services.ts
+++ frontend/src/app/components/shared/shared.service.ts/validate.services.ts
@@ -1,105 +1,94 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ValidateService {
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor(
-    private http: HttpClient,
-    private router: Router
-  ) { };
-
-  emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+-={}|[\]:";'<>,.?/~`]).{8,}$/;
-  nameRegex = /^[a-zA-Z]+\s[a-zA-Z]+(\s[a-zA-Z]+){0,8}$/
+  emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  passwordRegex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+-={}|[\]:";'<>,.?/~`]).{8,}$/
+  nameRegex =
+    /^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+\s[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+(\s[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ]+){0,8}$/
   cpfRegex = /^\d{11}$/
 
-  
-  colorInput(valid:boolean, input: HTMLInputElement) {
-    
+  colorInput(valid: boolean, input: HTMLInputElement) {
     // Verifica se o email é válido
     if (valid) {
-        input.style.borderBottomColor = 'green'
-    }else{
-        input.style.borderBottomColor = 'red'
+      input.style.borderBottomColor = 'green'
+    } else {
+      input.style.borderBottomColor = 'red'
     }
   }
-  validateEmailData(email:string) {
-    
+  validateEmailData(email: string) {
     // Verifica se o email é válido
     if (this.emailRegex.test(email)) {
-        return true
-    } else{
-        return false
+      return true
+    } else {
+      return false
     }
   }
 
-  validatePasswordData(password:string) {
-    
+  validatePasswordData(password: string) {
     // Verifica se a senha é válido
     if (this.passwordRegex.test(password)) {
-        return true
-    } else{
-        return false
+      return true
+    } else {
+      return false
     }
   }
 
-  validateNameData(name:string) {
-    
+  validateNameData(name: string) {
     // Verifica se a senha é válido
     if (this.nameRegex.test(name)) {
-       
-        return true
-    } else{
-
-        return false
+      return true
+    } else {
+      return false
     }
   }
 
-  validateCpfData(cpf:string) {
+  validateCpfData(cpf: string) {
+    cpf = cpf.replace(/[^\d]/g, '') // Remove caracteres não numéricos
 
-    cpf = cpf.replace(/[^\d]/g, ''); // Remove caracteres não numéricos
-    
     // Verifica se a senha é válido
     if (!this.cpfRegex.test(cpf)) {
-        return false
+      return false
     }
     if (/^(\d)\1+$/.test(cpf)) {
-      return false;
+      return false
     }
 
-    
-  // Calcula o primeiro dígito verificador
-  let sum = 0;
-  for (let i = 0; i < 9; i++) {
-    sum += parseInt(cpf.charAt(i)) * (10 - i);
-  }
-  let digit1 = 11 - (sum % 11);
-  if (digit1 > 9) {
-    digit1 = 0;
-  }
+    // Calcula o primeiro dígito verificador
+    let sum = 0
+    for (let i = 0; i < 9; i++) {
+      sum += parseInt(cpf.charAt(i)) * (10 - i)
+    }
+    let digit1 = 11 - (sum % 11)
+    if (digit1 > 9) {
+      digit1 = 0
+    }
 
-  // Calcula o segundo dígito verificador
-  sum = 0;
-  for (let i = 0; i < 10; i++) {
-    sum += parseInt(cpf.charAt(i)) * (11 - i);
-  }
-  let digit2 = 11 - (sum % 11);
-  if (digit2 > 9) {
-    digit2 = 0;
-  }
+    // Calcula o segundo dígito verificador
+    sum = 0
+    for (let i = 0; i < 10; i++) {
+      sum += parseInt(cpf.charAt(i)) * (11 - i)
+    }
+    let digit2 = 11 - (sum % 11)
+    if (digit2 > 9) {
+      digit2 = 0
+    }
 
-  // Verifica se os dígitos verificadores calculados são iguais aos dígitos do CPF
-  if (digit1 !== parseInt(cpf.charAt(9)) || digit2 !== parseInt(cpf.charAt(10))) {
-    return false;
-  }
+    // Verifica se os dígitos verificadores calculados são iguais aos dígitos do CPF
+    if (
+      digit1 !== parseInt(cpf.charAt(9)) ||
+      digit2 !== parseInt(cpf.charAt(10))
+    ) {
+      return false
+    }
 
-  return true;
+    return true
   }
-
-  
 }
