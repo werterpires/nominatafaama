2b7078de311995b4c21c2312644f858a20ad91cb
Werter Pires <werterpires23@hotmail.com>
2023-07-22 19:51:19 -0300

    add migragtion professors to nominata

---
+++ backend/migrations/20230416000635_create_nominatas_professors.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTableIfNotExists('nominatas_professors', (table) => {
+    table.increments('nominata_student_id').primary()
+    table.integer('nominata_id').unsigned().notNullable()
+    table.integer('professor_id').unsigned().notNullable()
+
+    table.foreign('nominata_id').references('nominatas.nominata_id')
+    table.foreign('professor_id').references('professors.professor_id')
+
+    table.timestamps(true, true)
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable('nominatas_professors')
+}
