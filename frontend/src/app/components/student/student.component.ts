b363f854407fedafd5a135c4b621759cf8630f9d
Werter Pires <werterpires23@hotmail.com>
2023-09-04 06:01:17 -0300

    add download button

--- frontend/src/app/components/student/student.component.ts
+++ frontend/src/app/components/student/student.component.ts
@@ -40,6 +40,37 @@ export class StudentComponent {
   firstName!: string
   surname!: string
 
+  links: { [key: string]: string } = {
+    l13: '1or8iTtUzx6lmZWPJTss7yrQpDIYe5m-5',
+    l17: '1U-V-DRenG3lZWDsWqbhkzTnnjcoOAaKz',
+    l26: '15M7S6kXklZHIKb6xS6MabH9q1L-3Aa03',
+    l24: '143HsIDY_GesY4q6Fjyt08p4HfSjvyQ54',
+    l22: '1QMyw1FyKNOGFOrZa-d4bEM8W7bvTL9QA',
+    l14: '1oXubzs-nuMftIBgBXOH3T_xltWphCFZe',
+    l15: '11o3ZnrjikEQh-wSuuDBXfkHN3jZGpWWx',
+    l23: '1Dsdoi3Sre19oujWvhMGg0TTa6M1oKmod',
+    l10: '139NILPcQIYyrc1Qje0zOBfQXosBrWkK7',
+    l20: '17U3D4YyWRL0VoVMQEvzQEMHzyO2utLvB',
+    l182: '14Eq3kcjCKlt9tGwAJLvspjULyar4Jq7A',
+    l18: '1bKiTKo2rWUe5Mto7Mnzm7T7U9eC0ECGf',
+    l19: '1CNtH5tuqtCijXY8-kyi8Cwh1Huiv_YRa',
+    l11: '1v0Sd1JxSpg-NK-qVJ4BDbgIwKbyqObC8',
+    l8: '1jKYGV1-T8gDQHVqO-0mjiRt3ybCzmJDB',
+    l136: '1XaHDiJvBcAnhxbJul19vY07p226Vp4lT',
+    l101: '1Rglx6AOl5POuLKPC3zRPLzROZWetbAUi',
+    l21: '1XjfOLvhvVKOt8yIqGGhCjEuiDq5BW112',
+    l27: '1noLV10wCHZVUGeJuzvjlP38xNF2eckY0',
+    l5: '1E9zijDoqNaAhKbfpYPGFfv3bkgPcDIKN',
+    l12: '1ldUU9MBsOwKRuLD48LG7Ilw_MKFUR20H',
+    l178: '1Fp6p1sW-lCDZnq1jg9p61QugpCSkvxuQ',
+    l9: '1jsqZyhbf5Dcb3LudsNoZL3de61Fu3G83',
+    l32: '11Uo8RGq7d4ocHVLVvNB-98DhjxSjFHb7',
+    l28: '1V7ZkiKlBDB27T2-tuUATns5Uc7VPILS6',
+    l29: '1sc3ojBzBBuQsp6GwAFdXsvIW_ax9vwKc',
+  }
+
+  curriculumLink!: string
+
   constructor(
     private route: ActivatedRoute,
     private service: StudentService,
@@ -108,6 +139,10 @@ export class StudentComponent {
           this.student.photos?.family_photo?.file.data,
           'family',
         )
+        console.log(this.student.user?.cpf)
+        const key = 'l' + this.student.student?.person_id
+        this.curriculumLink =
+          'https://drive.google.com/uc?export=download&id=' + this.links[key]
         this.dataService.receiveStudent(
           this.student,
           this.alonePhoto,
