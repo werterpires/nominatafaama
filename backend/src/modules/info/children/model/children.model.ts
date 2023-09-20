a8f54b5c3c9fb0ba7d0b193467498f98ea4fe914
werterpires <werterpires23@hotmail.com>
2023-09-20 15:05:02 -0300

    config eslint and prettier

--- backend/src/modules/info/children/model/children.model.ts
+++ backend/src/modules/info/children/model/children.model.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@nestjs/common'
-import { Knex } from 'knex'
-import { InjectModel } from 'nest-knexjs'
-import { ICreateChild, IChild, IUpdateChild } from '../types/types'
+import { Injectable } from '@nestjs/common';
+import { Knex } from 'knex';
+import { InjectModel } from 'nest-knexjs';
+import { ICreateChild, IChild, IUpdateChild } from '../types/types';
 
 @Injectable()
 export class ChildrenModel {
   constructor(@InjectModel() private readonly knex: Knex) {}
 
   async createChild(createChildData: ICreateChild): Promise<number> {
-    let child_id!: number
-    let sentError: Error | null = null
+    let child_id!: number;
+    let sentError: Error | null = null;
 
     await this.knex.transaction(async (trx) => {
       try {
@@ -21,13 +21,13 @@ export class ChildrenModel {
           child_approved,
           name,
           cpf,
-        } = createChildData
+        } = createChildData;
 
         const [person_id] = await trx('people')
           .insert({ name, cpf })
-          .returning('person_id')
+          .returning('person_id');
 
-        ;[child_id] = await trx('children')
+        [child_id] = await trx('children')
           .insert({
             child_birth_date,
             study_grade,
@@ -36,34 +36,34 @@ export class ChildrenModel {
             student_id,
             child_approved,
           })
-          .returning('child_id')
+          .returning('child_id');
 
-        await trx.commit()
+        await trx.commit();
       } catch (error) {
-        console.error(error)
-        await trx.rollback()
+        console.error(error);
+        await trx.rollback();
         if (error.code === 'ER_DUP_ENTRY') {
-          sentError = new Error('Child already exists')
+          sentError = new Error('Child already exists');
         } else {
-          sentError = new Error(error.sqlMessage)
+          sentError = new Error(error.sqlMessage);
         }
       }
-    })
+    });
 
     if (sentError) {
-      throw sentError
+      throw sentError;
     }
 
     if (!child_id) {
-      throw new Error('Failed to create child')
+      throw new Error('Failed to create child');
     }
 
-    return child_id
+    return child_id;
   }
 
   async findChildById(id: number): Promise<IChild> {
-    let child: IChild | null = null
-    let sentError: Error | null = null
+    let child: IChild | null = null;
+    let sentError: Error | null = null;
 
     await this.knex.transaction(async (trx) => {
       try {
@@ -73,18 +73,18 @@ export class ChildrenModel {
             'children.*',
             'marital_status_types.marital_status_type_name',
             'people.name',
-            'people.cpf',
+            'people.cpf'
           )
           .leftJoin(
             'marital_status_types',
             'children.marital_status_id',
-            'marital_status_types.marital_status_type_id',
+            'marital_status_types.marital_status_type_id'
           )
           .leftJoin('people', 'children.person_id', 'people.person_id')
-          .where('child_id', '=', id)
+          .where('child_id', '=', id);
 
         if (!result) {
-          throw new Error('Child not found')
+          throw new Error('Child not found');
         }
 
         child = {
@@ -100,31 +100,31 @@ export class ChildrenModel {
           cpf: result.cpf,
           created_at: result.created_at,
           updated_at: result.updated_at,
-        }
+        };
 
-        await trx.commit()
+        await trx.commit();
       } catch (error) {
-        console.error(error)
+        console.error(error);
 
-        sentError = new Error(error.message)
-        await trx.rollback()
+        sentError = new Error(error.message);
+        await trx.rollback();
       }
-    })
+    });
 
     if (sentError) {
-      throw sentError
+      throw sentError;
     }
 
     if (child === null) {
-      throw new Error('Child not found')
+      throw new Error('Child not found');
     }
 
-    return child
+    return child;
   }
 
   async findAllChildren(): Promise<IChild[]> {
-    let childrenList: IChild[] = []
-    let sentError: Error | null = null
+    let childrenList: IChild[] = [];
+    let sentError: Error | null = null;
 
     await this.knex.transaction(async (trx) => {
       try {
@@ -134,14 +134,14 @@ export class ChildrenModel {
             'children.*',
             'marital_status_types.marital_status_type_name',
             'people.name',
-            'people.cpf',
+            'people.cpf'
           )
           .leftJoin('people', 'children.person_id', 'people.person_id')
           .leftJoin(
             'marital_status_types',
             'children.marital_status_id',
-            'marital_status_types.marital_status_type_id',
-          )
+            'marital_status_types.marital_status_type_id'
+          );
 
         childrenList = results.map((row: any) => ({
           child_id: row.child_id,
@@ -156,48 +156,48 @@ export class ChildrenModel {
           cpf: row.cpf,
           created_at: row.created_at,
           updated_at: row.updated_at,
-        }))
+        }));
 
-        await trx.commit()
+        await trx.commit();
       } catch (error) {
-        console.error(error)
-        await trx.rollback()
-        sentError = new Error(error.sqlMessage)
+        console.error(error);
+        await trx.rollback();
+        sentError = new Error(error.sqlMessage);
       }
-    })
+    });
 
     if (sentError) {
-      throw sentError
+      throw sentError;
     }
 
-    return childrenList
+    return childrenList;
   }
 
   async findAllNotApprovedPersonIds(): Promise<{ person_id: number }[] | null> {
-    let personIds: { person_id: number }[] | null = null
-    let sentError: Error | null = null
+    let personIds: { person_id: number }[] | null = null;
+    let sentError: Error | null = null;
 
     try {
       const studentResult = await this.knex
         .table('children')
         .join('students', 'students.student_id', 'children.student_id')
         .select('students.person_id')
-        .whereNull('child_approved')
+        .whereNull('child_approved');
 
       personIds = [...studentResult].map((row) => ({
         person_id: row.person_id,
-      }))
+      }));
     } catch (error) {
-      console.error('Erro capturado na model: ', error)
-      sentError = new Error(error.message)
+      console.error('Erro capturado na model: ', error);
+      sentError = new Error(error.message);
     }
 
-    return personIds
+    return personIds;
   }
 
   async findChildrenByStudentId(student_id: number): Promise<IChild[]> {
-    let childrenList: IChild[] = []
-    let sentError: Error | null = null
+    let childrenList: IChild[] = [];
+    let sentError: Error | null = null;
 
     await this.knex.transaction(async (trx) => {
       try {
@@ -207,15 +207,15 @@ export class ChildrenModel {
             'children.*',
             'marital_status_types.marital_status_type_name',
             'people.name',
-            'people.cpf',
+            'people.cpf'
           )
           .leftJoin('people', 'children.person_id', 'people.person_id')
           .leftJoin(
             'marital_status_types',
             'children.marital_status_id',
-            'marital_status_types.marital_status_type_id',
+            'marital_status_types.marital_status_type_id'
           )
-          .where('children.student_id', '=', student_id)
+          .where('children.student_id', '=', student_id);
 
         childrenList = results.map((row: any) => ({
           child_id: row.child_id,
@@ -230,26 +230,26 @@ export class ChildrenModel {
           cpf: row.cpf,
           created_at: row.created_at,
           updated_at: row.updated_at,
-        }))
+        }));
 
-        await trx.commit()
+        await trx.commit();
       } catch (error) {
-        console.error(error)
-        await trx.rollback()
-        sentError = new Error(error.sqlMessage)
+        console.error(error);
+        await trx.rollback();
+        sentError = new Error(error.sqlMessage);
       }
-    })
+    });
 
     if (sentError) {
-      throw sentError
+      throw sentError;
     }
 
-    return childrenList
+    return childrenList;
   }
 
   async findApprovedChildrenByStudentId(student_id: number): Promise<IChild[]> {
-    let childrenList: IChild[] = []
-    let sentError: Error | null = null
+    let childrenList: IChild[] = [];
+    let sentError: Error | null = null;
 
     await this.knex.transaction(async (trx) => {
       try {
@@ -259,16 +259,16 @@ export class ChildrenModel {
             'children.*',
             'marital_status_types.marital_status_type_name',
             'people.name',
-            'people.cpf',
+            'people.cpf'
           )
           .leftJoin('people', 'children.person_id', 'people.person_id')
           .leftJoin(
             'marital_status_types',
             'children.marital_status_id',
-            'marital_status_types.marital_status_type_id',
+            'marital_status_types.marital_status_type_id'
           )
           .where('children.student_id', '=', student_id)
-          .andWhere('children.child_approved', '=', true)
+          .andWhere('children.child_approved', '=', true);
 
         childrenList = results.map((row: any) => ({
           child_id: row.child_id,
@@ -283,26 +283,26 @@ export class ChildrenModel {
           cpf: row.cpf,
           created_at: row.created_at,
           updated_at: row.updated_at,
-        }))
+        }));
 
-        await trx.commit()
+        await trx.commit();
       } catch (error) {
-        console.error(error)
-        await trx.rollback()
-        sentError = new Error(error.sqlMessage)
+        console.error(error);
+        await trx.rollback();
+        sentError = new Error(error.sqlMessage);
       }
-    })
+    });
 
     if (sentError) {
-      throw sentError
+      throw sentError;
     }
 
-    return childrenList
+    return childrenList;
   }
 
   async updateChildById(updateChild: IUpdateChild): Promise<number> {
-    let updatedChild: number | null = null
-    let sentError: Error | null = null
+    let updatedChild: number | null = null;
+    let sentError: Error | null = null;
 
     await this.knex.transaction(async (trx) => {
       try {
@@ -316,14 +316,14 @@ export class ChildrenModel {
           child_approved,
           name,
           cpf,
-        } = updateChild
+        } = updateChild;
 
         let approved = await trx('children')
           .first('child_approved')
-          .where('child_id', child_id)
+          .where('child_id', child_id);
 
         if (approved.child_approved == true) {
-          throw new Error('Registro já aprovado')
+          throw new Error('Registro já aprovado');
         }
 
         const updatedPerson = await trx('people')
@@ -331,7 +331,7 @@ export class ChildrenModel {
           .update({
             name,
             cpf,
-          })
+          });
 
         updatedChild = await trx('children')
           .where('child_id', child_id)
@@ -342,58 +342,58 @@ export class ChildrenModel {
             person_id,
             student_id,
             child_approved,
-          })
+          });
 
-        await trx.commit()
+        await trx.commit();
       } catch (error) {
-        console.error(error)
-        await trx.rollback()
-        sentError = new Error(error.message)
+        console.error(error);
+        await trx.rollback();
+        sentError = new Error(error.message);
       }
-    })
+    });
 
     if (sentError) {
-      throw sentError
+      throw sentError;
     }
 
     if (updatedChild === null) {
-      throw new Error('Child not found')
+      throw new Error('Child not found');
     }
 
-    return updatedChild
+    return updatedChild;
   }
 
   async deleteChildById(id: number): Promise<string> {
-    let sentError: Error | null = null
-    let message: string = ''
+    let sentError: Error | null = null;
+    let message: string = '';
 
     await this.knex.transaction(async (trx) => {
       try {
         const existingChild = await trx('children')
           .select('child_id', 'person_id')
           .where('child_id', id)
-          .first()
+          .first();
 
         if (!existingChild) {
-          throw new Error('Child not found')
+          throw new Error('Child not found');
         }
 
-        await trx('children').where('child_id', existingChild.child_id).del()
-        await trx('people').where('person_id', existingChild.person_id).del()
+        await trx('children').where('child_id', existingChild.child_id).del();
+        await trx('people').where('person_id', existingChild.person_id).del();
 
-        await trx.commit()
+        await trx.commit();
       } catch (error) {
-        console.error(error)
-        sentError = new Error(error.message)
-        await trx.rollback()
+        console.error(error);
+        sentError = new Error(error.message);
+        await trx.rollback();
       }
-    })
+    });
 
     if (sentError) {
-      throw sentError
+      throw sentError;
     }
 
-    message = 'Child deleted successfully'
-    return message
+    message = 'Child deleted successfully';
+    return message;
   }
 }
