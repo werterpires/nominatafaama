e970259aa7509867d2dd65d0533b36474d2a7282
werterpires <werterpires23@hotmail.com>
2023-11-01 14:18:51 -0300

    add notification to create, edit and delete evang exp types

--- backend/src/modules/resources/evang-exp-types/controllers/evang-exp-types.controller.ts
+++ backend/src/modules/resources/evang-exp-types/controllers/evang-exp-types.controller.ts
@@ -8,13 +8,15 @@ import {
   Delete,
   InternalServerErrorException,
   NotFoundException,
-  Put,
+  Put
 } from '@nestjs/common'
-import {EvangExpTypesService} from '../services/evang-exp-types.service'
-import {CreateEvangExpTypeDto} from '../dto/create-evang-exp-type.dto'
-import {UpdateEvangExpTypeDto} from '../dto/update-evang-exp-type.dto'
-import {ERoles} from 'src/shared/auth/types/roles.enum'
-import {Roles} from 'src/shared/roles/fz_decorators/roles.decorator'
+import { EvangExpTypesService } from '../services/evang-exp-types.service'
+import { CreateEvangExpTypeDto } from '../dto/create-evang-exp-type.dto'
+import { UpdateEvangExpTypeDto } from '../dto/update-evang-exp-type.dto'
+import { ERoles } from 'src/shared/auth/types/roles.enum'
+import { Roles } from 'src/shared/roles/fz_decorators/roles.decorator'
+import { UserFromJwt } from 'src/shared/auth/types/types'
+import { CurrentUser } from 'src/shared/auth/decorators/current-user.decorator'
 
 @Controller('evang-exp-types')
 export class EvangExpTypesController {
@@ -22,10 +24,14 @@ export class EvangExpTypesController {
 
   @Roles(ERoles.ADMINISTRACAO, ERoles.SECRETARIA, ERoles.DIRECAO)
   @Post()
-  async createEvangExpType(@Body() input: CreateEvangExpTypeDto) {
+  async createEvangExpType(
+    @Body() input: CreateEvangExpTypeDto,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
       const evangExpType = await this.evangExpTypesService.createEvangExpType(
         input,
+        currentUser
       )
       return evangExpType
     } catch (error) {
@@ -37,7 +43,7 @@ export class EvangExpTypesController {
   async getEvangExpTypeById(@Param('id') id: number) {
     try {
       const evangExpType = await this.evangExpTypesService.findEvangExpTypeById(
-        id,
+        id
       )
       if (!evangExpType) {
         throw new NotFoundException(`EvangExp type with id ${id} not found.`)
@@ -54,10 +60,16 @@ export class EvangExpTypesController {
   }
 
   @Put()
-  async updateEvangExpType(@Body() input: UpdateEvangExpTypeDto) {
+  async updateEvangExpType(
+    @Body() input: UpdateEvangExpTypeDto,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
       const updatedEvangExpType =
-        await this.evangExpTypesService.updateEvangExpTypeById(input)
+        await this.evangExpTypesService.updateEvangExpTypeById(
+          input,
+          currentUser
+        )
       return updatedEvangExpType
     } catch (error) {
       throw new InternalServerErrorException(error.message)
@@ -65,10 +77,16 @@ export class EvangExpTypesController {
   }
 
   @Delete(':id')
-  async deleteEvangExpType(@Param('id') id: number) {
+  async deleteEvangExpType(
+    @Param('id') id: number,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
-      const message = await this.evangExpTypesService.deleteEvangExpTypeById(id)
-      return {message}
+      const message = await this.evangExpTypesService.deleteEvangExpTypeById(
+        id,
+        currentUser
+      )
+      return { message }
     } catch (error) {
       throw new InternalServerErrorException(error.message)
     }
