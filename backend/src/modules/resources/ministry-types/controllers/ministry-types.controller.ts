1cccf5694f9e56618c9bdb977ab58677461fca57
werterpires <werterpires23@hotmail.com>
2023-11-01 14:47:42 -0300

    add notification to create ministry type

--- backend/src/modules/resources/ministry-types/controllers/ministry-types.controller.ts
+++ backend/src/modules/resources/ministry-types/controllers/ministry-types.controller.ts
@@ -1,66 +1,95 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, InternalServerErrorException, NotFoundException, Put } from '@nestjs/common';
-import { MinistryTypesService } from '../services/ministry-types.service';
-import { CreateMinistryTypeDto } from '../dto/create-ministry-type.dto';
-import { UpdateMinistryTypeDto } from '../dto/update-ministry-type.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  InternalServerErrorException,
+  NotFoundException,
+  Put
+} from '@nestjs/common'
+import { MinistryTypesService } from '../services/ministry-types.service'
+import { CreateMinistryTypeDto } from '../dto/create-ministry-type.dto'
+import { UpdateMinistryTypeDto } from '../dto/update-ministry-type.dto'
+import { UserFromJwt } from 'src/shared/auth/types/types'
+import { CurrentUser } from 'src/shared/auth/decorators/current-user.decorator'
 
 @Controller('ministry-types')
 export class MinistryTypesController {
   constructor(private readonly ministryTypesService: MinistryTypesService) {}
 
   @Post()
-  async createMinistryType(@Body() input: CreateMinistryTypeDto) {
+  async createMinistryType(
+    @Body() input: CreateMinistryTypeDto,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
-      const ministryType = await this.ministryTypesService.createMinistryType(input);
-      return ministryType;
+      const ministryType = await this.ministryTypesService.createMinistryType(
+        input,
+        currentUser
+      )
+      return ministryType
     } catch (error) {
-      throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message)
     }
   }
 
   @Get(':id')
   async getMinistryTypeById(@Param('id') id: number) {
     try {
-      const ministryType = await this.ministryTypesService.findMinistryTypeById(id);
+      const ministryType = await this.ministryTypesService.findMinistryTypeById(
+        id
+      )
       if (!ministryType) {
-        throw new NotFoundException(`Ministry type with id ${id} not found.`);
+        throw new NotFoundException(`Ministry type with id ${id} not found.`)
       }
-      return ministryType;
+      return ministryType
     } catch (error) {
-      throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message)
     }
   }
 
   @Get()
   async findAllMinistryTypes() {
-    try{
-      return await this.ministryTypesService.findAllMinistryTypes();
-    }catch(error){
+    try {
+      return await this.ministryTypesService.findAllMinistryTypes()
+    } catch (error) {
       throw error
     }
-    
   }
 
   @Put()
-  async updateMinistryType(@Body() input: UpdateMinistryTypeDto) {
+  async updateMinistryType(
+    @Body() input: UpdateMinistryTypeDto,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
-      const updatedMinistryType = await this.ministryTypesService.updateMinistryTypeById(input);
-      return updatedMinistryType;
+      const updatedMinistryType =
+        await this.ministryTypesService.updateMinistryTypeById(
+          input,
+          currentUser
+        )
+      return updatedMinistryType
     } catch (error) {
-      throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message)
     }
   }
 
   @Delete(':id')
-  async deleteMinistryType(@Param('id') id: number) {
+  async deleteMinistryType(
+    @Param('id') id: number,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
-      const message = await this.ministryTypesService.deleteMinistryTypeById(id);
-      return { message };
+      const message = await this.ministryTypesService.deleteMinistryTypeById(
+        id,
+        currentUser
+      )
+      return { message }
     } catch (error) {
-      throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message)
     }
   }
-
-
-
 }
-
--- backend/src/modules/resources/ministry-types/ministry-types.module.ts
+++ backend/src/modules/resources/ministry-types/ministry-types.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
-import { MinistryTypesService } from './services/ministry-types.service';
-import { MinistryTypesController } from './controllers/ministry-types.controller';
-import { MinistryTypesModel } from './model/ministry-types.model';
+import { Module } from '@nestjs/common'
+import { MinistryTypesService } from './services/ministry-types.service'
+import { MinistryTypesController } from './controllers/ministry-types.controller'
+import { MinistryTypesModel } from './model/ministry-types.model'
+import { NotificationsModule } from 'src/shared/notifications/notifications.module'
 
 const services = [MinistryTypesService, MinistryTypesModel]
 
 @Module({
+  imports: [NotificationsModule],
   controllers: [MinistryTypesController],
   providers: services,
   exports: services
