83e936dd29b111777afa6ef9462bbc8d8300830d
werterpires <werterpires23@hotmail.com>
2023-11-01 13:52:45 -0300

    add notifications to create, edit and delete marital status

--- backend/src/modules/resources/marital-status/controllers/marital-status.controller.ts
+++ backend/src/modules/resources/marital-status/controllers/marital-status.controller.ts
@@ -1,59 +1,93 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, InternalServerErrorException, NotFoundException, Put } from '@nestjs/common';
-import { MaritalStatusService } from '../services/marital-status.service';
-import { CreateMaritalStatusDto } from '../dto/create-marital-status.dto';
-import { UpdateMaritalStatusDto } from '../dto/update-marital-status.dto';
-import { Roles } from 'src/shared/roles/fz_decorators/roles.decorator';
-import { ERoles } from 'src/shared/auth/types/roles.enum';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  InternalServerErrorException,
+  NotFoundException,
+  Put
+} from '@nestjs/common'
+import { MaritalStatusService } from '../services/marital-status.service'
+import { CreateMaritalStatusDto } from '../dto/create-marital-status.dto'
+import { UpdateMaritalStatusDto } from '../dto/update-marital-status.dto'
+import { Roles } from 'src/shared/roles/fz_decorators/roles.decorator'
+import { ERoles } from 'src/shared/auth/types/roles.enum'
+import { UserFromJwt } from 'src/shared/auth/types/types'
+import { CurrentUser } from 'src/shared/auth/decorators/current-user.decorator'
 
 @Controller('marital-status')
 export class MaritalStatusController {
   constructor(private readonly maritalStatusService: MaritalStatusService) {}
   @Roles(ERoles.ADMINISTRACAO, ERoles.SECRETARIA, ERoles.DIRECAO)
   @Post()
-  async createMaritalStatus(@Body() input: CreateMaritalStatusDto) {
+  async createMaritalStatus(
+    @Body() input: CreateMaritalStatusDto,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
-      const maritalStatus = await this.maritalStatusService.createMaritalStatus(input);
-      return maritalStatus;
+      const maritalStatus = await this.maritalStatusService.createMaritalStatus(
+        input,
+        currentUser
+      )
+      return maritalStatus
     } catch (error) {
-      throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message)
     }
   }
 
   @Get(':id')
   async getMaritalStatusById(@Param('id') id: number) {
     try {
-      const maritalStatus = await this.maritalStatusService.findMaritalStatusById(id);
+      const maritalStatus =
+        await this.maritalStatusService.findMaritalStatusById(id)
       if (!maritalStatus) {
-        throw new NotFoundException(`Marital status with id ${id} not found.`);
+        throw new NotFoundException(`Marital status with id ${id} not found.`)
       }
-      return maritalStatus;
+      return maritalStatus
     } catch (error) {
-      throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message)
     }
   }
 
   @Get()
   async findAllMaritalStatuses() {
-    return await this.maritalStatusService.findAllMaritalStatuses();
+    return await this.maritalStatusService.findAllMaritalStatuses()
   }
 
   @Put()
-  async updateMaritalStatusById(id: number, @Body() input: UpdateMaritalStatusDto) {
+  async updateMaritalStatusById(
+    id: number,
+    @Body() input: UpdateMaritalStatusDto,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
-      const updatedMaritalStatus = await this.maritalStatusService.updateMaritalStatusById(input);
-      return updatedMaritalStatus;
+      const updatedMaritalStatus =
+        await this.maritalStatusService.updateMaritalStatusById(
+          input,
+          currentUser
+        )
+      return updatedMaritalStatus
     } catch (error) {
-      throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message)
     }
   }
 
   @Delete(':id')
-  async deleteMaritalStatusById(@Param('id') id: number) {
+  async deleteMaritalStatusById(
+    @Param('id') id: number,
+    @CurrentUser() currentUser: UserFromJwt
+  ) {
     try {
-      const message = await this.maritalStatusService.deleteMaritalStatusById(id);
-      return { message };
+      const message = await this.maritalStatusService.deleteMaritalStatusById(
+        id,
+        currentUser
+      )
+      return { message }
     } catch (error) {
-      throw new InternalServerErrorException(error.message);
+      throw new InternalServerErrorException(error.message)
     }
   }
 }
