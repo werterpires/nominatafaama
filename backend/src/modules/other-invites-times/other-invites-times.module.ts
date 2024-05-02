6ee203ade356e038aab3633247e3d291fd492af2
werterpires <werterpires23@hotmail.com>
2024-05-02 09:19:56 -0300

    change other invites Time Module

--- backend/src/modules/other-invites-times/other-invites-times.module.ts
+++ backend/src/modules/other-invites-times/other-invites-times.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
 import { OtherInvitesTimesService } from './services/other-invites-times.service'
 import { OtherInvitesTimesController } from './controllers/other-invites-times.controller'
+import { OtherInvitesTimesModel } from './model/other-invites-times.model'
 
+const services = [OtherInvitesTimesService, OtherInvitesTimesModel]
 @Module({
   controllers: [OtherInvitesTimesController],
-  providers: [OtherInvitesTimesService]
+  providers: services,
+  exports: services
 })
 export class OtherInvitesTimesModule {}
